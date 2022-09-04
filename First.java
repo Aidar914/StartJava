d08fc1220777708a05d9052c5be11694c1864705
Aidar237 <74776153+Aidar914@users.noreply.github.com>
2022-09-04 21:03:22 +0300

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First
+{
+    public static void main(String[] arg) {
+        System.out.println("ФИО: Ахсетов Айдар Илькамович");
+        System.out.println("Группа: 11-205");
+        System.out.println("название института: ИТИС");
+        System.out.println("название университета: КФУ");
+    }
+}
